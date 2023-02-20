fd57a040a22b9418bae71539e50a56d06287e972
Krisjanis Dzenis <Krisjanis.Dzenis@edu.rtu.lv>
2023-02-20 15:59:33 +0200

    test2

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    d = 0
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next, i+1))
+            opening_brackets_stack.append(Bracket(next, d+1))
 
         if next in ")]}":
             # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
+                return d+1
             opening_brackets_stack.pop()
+
+        d = d + 1
         
 def main():
     text = input()
