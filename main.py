df276c2295d9f99239cbd6120b0411cd67ef556c
Krisjanis Dzenis <Krisjanis.Dzenis@edu.rtu.lv>
2023-02-23 12:24:17 +0200

    test

--- main.py
+++ main.py
@@ -14,25 +14,28 @@ def find_mismatch(text):
     d = 0
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            
             opening_brackets_stack.append(Bracket(next, d+1))
 
         if next in ")]}":
-            # Process closing bracket, write your code here
+           
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return d+1
             opening_brackets_stack.pop()
             
         d = d + 1
-        
+
+
 def main():
     text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    if not mismatch:
-        print("Success")
-    else:
-        print(mismatch)
+    if "I" in text:
+        text = input()
+        mismatch = find_mismatch(text)
+        # Printing answer, write your code here
+        if not mismatch:
+            print("Success")
+        else:
+            print(mismatch)
 
 if __name__ == "__main__":
     main()
